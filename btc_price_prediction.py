e9b3d63b307d459017d839726a9952db250ea919
StevenFelesky <feleskysm@g.cofc.edu>
2022-04-03 20:52:44 -0400

    updates

--- btc_price_prediction.py
+++ btc_price_prediction.py
@@ -7,4 +7,31 @@ Predict the price of bitcoin based on sentiment of headlines
 
 import csv
 import nltk
+from nltk.sentiment import SentimentIntensityAnalyzer
+
+# read csv file in 
+file = open('mw_headlines.csv')
+csvreader = csv.reader(file)
+data = []
+for row in csvreader:
+    data.append(row)
+file.close()
+
+scores = []
+
+# sentiment analysis
+sia = SentimentIntensityAnalyzer()
+for row in data:
+    score = 0
+    if len(row) > 1:
+        i = 1
+        while (i < len(row)):
+            ps = sia.polarity_scores(row[i])
+            score = score + ps['compound']
+            i += 1
+    scores.append([row[0], score])
+
+with open('mw_headline_scores.csv', 'w', encoding='UTF8', newline='') as f:
+    writer = csv.writer(f)
+    writer.writerows(scores)
 
